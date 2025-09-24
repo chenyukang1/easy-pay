768e6f5c0601dbda7291e73ccad1211c0b539e45
yukang.chen <chen.yukang@qq.com>
2025-09-24 11:49:53 +0800

    first commit

---
+++ easy-pay-starter/src/main/java/com/cyk/easy/pay/Application.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015-2025，千寻位置网络有限公司版权所有。
+ *
+ * 时空智能 共创数字中国（厘米级定位 | 毫米级感知 | 纳秒级授时）
+ */
+package com.cyk.easy.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yukang.chen
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
